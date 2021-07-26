49d8ca995851a1faa5d25cb6244a0c5eed166007
Doug Jones <DSJR1284@GMAIL.COM>
2021-07-26 14:39:34 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
 // your Bomb code here!
+
+import React, { Component } from 'react'
+
+export default class Bomb extends Component {
+    constructor(props){
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }    
+    // constructor(props) { //props that the component gets from its parent
+    //     super()
+    //     this.state = { //define initial state with a key of 'someKey' set to the 'someValue' prop
+    //       someKey: props.someValue
+    //     }
+    //   }
+    render() {
+        const gameOver = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+
+        return (
+            <div>{gameOver}</div>
+          )
+        }        
+    }
+
